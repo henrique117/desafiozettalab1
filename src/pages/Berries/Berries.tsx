fd7c95301d070e78f2965e4e46ce8065bafceaed
Henrique <hassis.moreira177@hotmail.com>
2025-10-27 13:21:01 -0300

    feat(berries): começo da página de berries

---
+++ src/pages/Berries/Berries.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { PageLayout } from '../../components/layout/layout.export'
+
+const Berries: React.FC = () => {
+    return (
+        <PageLayout>
+            <h1 className="fs-1 text-light">Berries</h1>
+        </PageLayout>
+    )
+}
+
+export default Berries
