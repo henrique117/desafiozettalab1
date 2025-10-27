e80516fcbce10d3d9e7585eafded938a55d3e8e0
Henrique <hassis.moreira177@hotmail.com>
2025-10-27 13:16:17 -0300

    feat(page layout): wrapper para colocar a navbar e o footer nas páginas

---
+++ src/components/layout/PageLayout/PageLayout.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Footer, NavBar } from '../layout.export'
+
+interface PageLayoutProps {
+    children: React.ReactNode
+}
+
+const PageLayout: React.FC<PageLayoutProps> = ({ children }) => {
+    return (
+        <div className="d-flex flex-column min-vh-100">
+            <NavBar />
+            <main className="flex-grow-1" style={{ paddingTop: '5rem' }}>
+                {children}
+            </main>
+            <Footer />
+        </div>
+    )
+}
+
+export default PageLayout
